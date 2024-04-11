9627c3f13dcc61d2acb7e0cfb84be3c2fb8ed871
Ashish SHUKLA <ashish.is@lostca.se>
2024-04-11 19:44:02 +0000

    Add tmux package

---
+++ guix/abbe/packages/tmux.scm
@@ -0,0 +1,21 @@
+(define-module (abbe packages tmux)
+  #:use-module (guix packages)
+  #:use-module (gnu packages tmux)
+  #:use-module (gnu packages bison)
+  #:use-module (guix download))
+
+(define-public tmux-34
+  (package
+    (inherit tmux)
+    (version "3.4")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://github.com/tmux/tmux/releases/download/"
+                    version "/tmux-" version ".tar.gz"))
+              (sha256
+               (base32
+		"1ahr7si3akr55hadyms3p36f1pbwavpkbfxpsq55ql5zl3gbh6jm"))))
+    (native-inputs
+     (modify-inputs (package-native-inputs tmux)
+       (prepend bison)))))
