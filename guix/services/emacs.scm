b490a5ff1ff3467d35ddcd3fc1a7bff3b3492640
Ashish SHUKLA <ashish.is@lostca.se>
2024-04-11 19:03:37 +0000

    Initialize channel with emacs-daemon

---
+++ guix/services/emacs.scm
@@ -0,0 +1,20 @@
+(define-module (packages emacs)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu home services shepherd)
+  #:use-module (guix gexp))
+
+(define-public emacs-daemon
+  (shepherd-service
+   (provision '(emacs-daemon))
+   (documentation "Run `emacs-daemon'")
+   (modules '((shepherd support)))
+   (start	#~(make-forkexec-constructor
+		   (list
+		    (string-append #$emacs-no-x "/bin/emacs")
+		    "--fg-daemon")
+		   #:log-file (string-append %user-log-dir "/emacs-daemon.log")))
+   (stop #~(make-kill-destructor))
+   (one-shot? #f)
+   (respawn? #t)))
