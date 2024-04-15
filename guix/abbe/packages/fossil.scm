d78897260948e8de7a7558de9f84ec7e2e1e901f
Ashish SHUKLA <ashish.is@lostca.se>
2024-04-15 11:49:15 +0000

    Add fnc

--- guix/abbe/packages/fossil.scm
+++ guix/abbe/packages/fossil.scm
@@ -19,3 +19,22 @@
     (inputs (modify-inputs (package-inputs fossil)
 	      (delete "sqlite")
 	      (append sqlite-next)))))
+
+(define-public fnc
+  (package
+    (name "fnc")
+    (version "0.16")
+    (source (origin
+              (method url-fetch)
+              (uri
+               (string-append "https://fnc.bsdbox.org/uv/dl/fnc-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1npnbdz5i4p61ri76vx6awggbc0q19y8b26l3sy4wxmaxkly7gwy"))))
+    (build-system trivial-build-system)
+    (build-inputs (list sqlite-next))
+    (home-page "https://fnc.bsdbox.org")
+    (synopsis "Interactive text-based user interface for Fossil")
+    (description "fnc uses ncurses and libfossil to create a fossil ui experience in
+the terminal, and parse local changes at the hunk level to prepare atomic commits.")
+    (license license:isc)))
