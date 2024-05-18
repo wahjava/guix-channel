126ea126b9cfe68dd9fa54e5c6e8367d2495e8fa
Ashish SHUKLA <ashish.is@lostca.se>
2024-05-18 06:27:59 +0000

    kakoune: update to 2024.05.18

--- guix/abbe/packages/kakoune.scm
+++ guix/abbe/packages/kakoune.scm
@@ -3,11 +3,11 @@
   #:use-module (guix packages)
   #:use-module (gnu packages text-editors))
 
-(define-public kakoune-2024-05-09
+(define-public kakoune-2024-05-18
   (package
    (inherit kakoune)
-   (name "kakoune-2024-05-09")
-   (version "2024.05.09")
+   (name "kakoune-2024-05-18")
+   (version "2024.05.18")
    (source
     (origin
      (method url-fetch)
@@ -15,4 +15,4 @@
                          "releases/download/v" version "/"
                          "kakoune-" version ".tar.bz2"))
      (sha256
-      (base32 "0ad22syhsd921rnfpmkvyh37am3ni443g1f3jc2hqndgsggvv411"))))))
+      (base32 "1ymr1jpdnd5wj6npzi8bgfd30d0j885sfnhl236rn7fjc4pars6s"))))))
