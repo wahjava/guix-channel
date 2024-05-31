08c9577fc3bd3ec28f534d372193aef1b5b2823e
Ashish SHUKLA <ashish.is@lostca.se>
2024-05-31 13:53:50 +0000

    weechat: Update to 4.3.1

--- guix/abbe/packages/weechat.scm
+++ guix/abbe/packages/weechat.scm
@@ -7,17 +7,17 @@
   #:use-module (guix utils)
   #:use-module ((guix licenses) :prefix license:))
 
-(define-public weechat-4-3-0
+(define-public weechat-4-3-1
   (package
     (inherit weechat)
     (name "weechat")
-    (version "4.3.0")
+    (version "4.3.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://weechat.org/files/src/weechat-"
                                   version ".tar.xz"))
               (sha256
                (base32
-                "0bc6jvvsprrpnr0bjfi6z0h0z3x0dkq5h38y9nipg7zny85zky9n"))))
+                "0lw6s5864nrdxp9bxzh5x1s68y0d8w21lqrraxk3qc6cgnhj4zhm"))))
     (inputs (modify-inputs (package-inputs weechat)
 			   (append cjson)))))
