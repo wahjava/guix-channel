7a994f4dddba542b01107e381f11768914dc934b
Ashish SHUKLA <ashish.is@lostca.se>
2024-06-06 19:45:11 +0200

    Update mcfly for multiple architectures

--- guix/abbe/packages/mcfly.scm
+++ guix/abbe/packages/mcfly.scm
@@ -4,24 +4,32 @@
   #:use-module (guix gexp)
   #:use-module (nonguix build-system binary)
   #:use-module (guix download)
+  #:use-module (ice-9 match)
   #:use-module (guix licenses))
 
-(define helix-version "24.03")
-(define helix-base-dir ".")
+(define (mcfly-arch system)
+  (match system
+    ("aarch64-linux" "aarch64")
+    ("x86_64-linux" "x86_64")))
+
+(define (mcfly-url version system)
+  (let ([system (mcfly-arch system)])
+    (string-append
+     "https://github.com/cantino/mcfly/releases/download/v" version "/mcfly-v" version "-" system "-unknown-linux-musl.tar.gz")))
+
+(define (mcfly-hash system)
+  (match system
+    ("aarch64-linux" "112jxqf75izvh04yk5wd4dy6mfi1ws55kqzrhw365d4qk1bb9lh5")
+    ("x86_64-linux" "0fvqr1fyskiajbp401m3rqlm3hxlik36r36kj3cc14y1wkcb8v5x")))
 
 (define-public mcfly
   (package
    (name "mcfly")
-   (version "0.8.6")
+   (version "0.9.0")
    (source (origin
             (method url-fetch/tarbomb)
-            (uri (string-append
-		  "https://github.com/cantino/mcfly/releases/download/v"
-		  version
-		  "/mcfly-v"
-		  version
-		  "-aarch64-unknown-linux-musl.tar.gz"))
-	    (sha256 (base32 "0sml4zr9qg8b5d6c9xi3vqi2cdqf74cib7l5czy08d918yh2nd8g"))))
+            (uri (mcfly-url version (%current-system)))
+            (sha256 (base32 (mcfly-hash (%current-system))))))
    (build-system binary-build-system)
    (arguments
     '(#:install-plan
