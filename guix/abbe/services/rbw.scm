5c18d7524d0c953dc04d772a739b729c41fec482
Ashish SHUKLA <ashish.is@lostca.se>
2024-06-09 22:53:08 +0200

    Fix service: rbw

--- guix/abbe/services/rbw.scm
+++ guix/abbe/services/rbw.scm
@@ -2,6 +2,7 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix gexp)
+  #:use-module (guix records)
   #:use-module (gnu home services)
   #:use-module (gnu home services xdg)
   #:use-module (gnu services)
