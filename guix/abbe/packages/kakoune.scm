eae4d9d49e20522639cba8cee8bf0fe85a515e08
Ashish SHUKLA <ashish.is@lostca.se>
2024-06-24 05:41:50 +0200

    Remove kakoune

--- guix/abbe/packages/kakoune.scm
+++ guix/abbe/packages/kakoune.scm
@@ -5,20 +5,6 @@
   #:use-module (guix build-system copy)
   #:use-module (gnu packages text-editors))
 
-(define-public kakoune-2024-05-18
-  (package
-   (inherit kakoune)
-   (name "kakoune")
-   (version "2024.05.18")
-   (source
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://github.com/mawww/kakoune/"
-                         "releases/download/v" version "/"
-                         "kakoune-" version ".tar.bz2"))
-     (sha256
-      (base32 "1ymr1jpdnd5wj6npzi8bgfd30d0j885sfnhl236rn7fjc4pars6s"))))))
-
 (define-public kak-lsp-17-0-1
   (package
    (name "kak-lsp")
