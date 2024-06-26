dc5df968315906d3a8296d82723087860353c3e1
Ashish SHUKLA <ashish.is@lostca.se>
2024-06-26 23:19:08 +0000

    go: add for arm64

--- guix/abbe/packages/go.scm
+++ guix/abbe/packages/go.scm
@@ -2,8 +2,18 @@
   #:use-module (guix packages)
   #:use-module (nonguix build-system binary)
   #:use-module (guix download)
+  #:use-module (ice-9 match)
   #:use-module (guix licenses))
 
+(define (go-hash)
+  (match (%current-system)
+    ("aarch64-linux" "0wg7z71f189z1qbs7v27ha7kxsi75rba7b1022va3r6jak1pgqd8")
+    ("x86_64-linux" "0zfypbkqbnsr66ghjaww0hg6b2j1raf265il4yb52mq2c59d8yds")))
+
+(define (go-file-suffix)
+  (match (%current-system)
+    ("aarch64-linux" "linux-arm64")
+    ("x86_64-linux" "linux-amd64")))
 
 (define-public go-122
   (package
@@ -11,8 +21,8 @@
    (version "1.22.4")
    (source (origin
             (method url-fetch)
-            (uri (string-append "https://go.dev/dl/go" version ".linux-amd64.tar.gz"))
-            (sha256 (base32 "0zfypbkqbnsr66ghjaww0hg6b2j1raf265il4yb52mq2c59d8yds"))))
+            (uri (string-append "https://go.dev/dl/go" version "." (go-file-suffix) ".tar.gz"))
+            (sha256 (base32 (go-hash)))))
    (build-system binary-build-system)
    (synopsis "Go language compiler")
    (description "Go language compiler")
