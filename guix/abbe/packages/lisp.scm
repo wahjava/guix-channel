1d278434d9e865361aa80785e7ad4153695fcedc
Ashish SHUKLA <ashish.is@lostca.se>
2024-07-06 00:50:06 +0200

    sbcl: Update to 2.4.6

--- guix/abbe/packages/lisp.scm
+++ guix/abbe/packages/lisp.scm
@@ -10,6 +10,20 @@
   (package
    (inherit sbcl)
    (name "sbcl-optimized")
+   (version "2.4.6")
+   (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://sourceforge/sbcl/sbcl/" version "/sbcl-"
+                           version "-source.tar.bz2"))
+       (sha256
+        (base32 "149h7xyadg13f3xs3v7klap0lrsngazqaafn4qbrmq6s89w912d4"))
+       (modules '((guix build utils)))
+       (snippet
+        '(begin
+           ;; Don't force ARMv5.
+           (substitute* "src/runtime/Config.arm-linux"
+             (("-march=armv5t") ""))))))
    (native-inputs
     (modify-inputs (package-native-inputs sbcl)
                    (delete "clisp")
