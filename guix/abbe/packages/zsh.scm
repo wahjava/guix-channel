6a76e63a751686a27af84bbdce6eb5a68473080c
Ashish SHUKLA <ashish.is@lostca.se>
2024-07-16 06:12:28 +0200

    zsh: add new plugin zsh-history-filter

--- guix/abbe/packages/zsh.scm
+++ guix/abbe/packages/zsh.scm
@@ -61,3 +61,27 @@
                                  "powerlevel9k.zsh-theme"
                                  "prompt_powerlevel10k_setup"
                                  "prompt_powerlevel9k_setup"))))))
+
+(define-public zsh-history-filter
+  (package
+   (name "zsh-history-filter")
+   (version "0.4.1-20210405")
+   (source
+    (origin (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/MichaelAquilina/zsh-history-filter")
+                  (commit "49d7987f093374fb72f0f7eef2d63e641085a226")))
+            (file-name (git-file-name name version))
+            (sha256 (base32 "1avkhh7dhbw3kijvhs6xs8b4xc273xprsxrhmsmxh4ha7vibyahd"))))
+   (build-system copy-build-system)
+   (home-page "https://github.com/MichaelAquilina/zsh-history-filter/")
+   (synopsis "Zsh plugin to filter out some commands from being added to you history")
+   (description
+    "zsh plugin that allows you to specify patterns that will automatically exclude commands from being inserted into your permanent history. Particularly useful for preventing secrets being written.")
+   (license license:gpl3)
+   (arguments (list #:install-plan
+                    #~(map (lambda (d)
+                             (list d
+                                   "share/zsh/plugins/zsh-history-filter/"))
+                           (list "README.rst"
+                                 "zsh-history-filter.plugin.zsh"))))))
