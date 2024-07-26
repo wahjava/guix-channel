4fca3986f9637b267ab2569553fdf013d3617c86
Ashish SHUKLA <ashish.is@lostca.se>
2024-07-26 21:24:52 +0200

    Add fossil-bin

--- guix/abbe/packages/fossil.scm
+++ guix/abbe/packages/fossil.scm
@@ -1,10 +1,12 @@
 (define-module (abbe packages fossil)
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages ncurses)
   #:use-module (gnu packages compression)
+  #:use-module (nonguix build-system binary)
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix utils)
@@ -13,35 +15,55 @@
 (define-public fossil-2-24
   (define (fossil-patch name from to hash)
     (origin (method url-fetch)
-	    (uri (string-append "https://fossil-scm.org/home/vpatch?from=" from
-				"&to=" to))
-	    (sha256 (base32 hash))
-	    (file-name name)))
+      (uri (string-append "https://fossil-scm.org/home/vpatch?from=" from
+            "&to=" to))
+      (sha256 (base32 hash))
+      (file-name name)))
   (package
     (inherit fossil)
     (name "fossil")
     (version "2.24")
     (source (origin
-	      (method url-fetch)
-	      (uri
-	       (string-append
-		"https://fossil-scm.org/home/tarball/version-" version "/fossil-" version ".tar.gz"))
-	      (sha256
-	       (base32 "0k1gjvxbvvs98yspnf7nj6rb0c7yf60savq7jywbkgimd0bkrkcm"))
-	      (patch-flags '("-p0"))
-	      (patches (list
-			(fossil-patch "fossil-disable-tests.patch"
-				      "8be0372c10510437"
-				      "5ad708085a90365f"
-				      "1b4svsrz7cr1zi6qfpavj3ddm6dr0966jbgkbvgjz79ljqmpiasf")
-			(fossil-patch "fossil-fix-json-test.patch"
-				      "fb4e90b662803e47"
-				      "17c01c549e73c6b8"
-				      "12gjzyxs22g9grv7qbgp9jg133bgcsj74621s05fk82j3fc7z59g")
-			(fossil-patch "fossil-comment-utf-tests.patch"
-				      "5ad708085a90365f"
-				      "fb4e90b662803e47"
-				      "05h2mb6g0840yq74x1cdj95jmqb95i75h6g5v0rzqdc994b96cd4")))))
+             (method url-fetch)
+             (uri
+              (string-append
+               "https://fossil-scm.org/home/tarball/version-" version "/fossil-" version ".tar.gz"))
+             (sha256
+              (base32 "0k1gjvxbvvs98yspnf7nj6rb0c7yf60savq7jywbkgimd0bkrkcm"))
+             (patch-flags '("-p0"))
+             (patches (list
+                       (fossil-patch "fossil-disable-tests.patch"
+                        "8be0372c10510437"
+                        "5ad708085a90365f"
+                        "1b4svsrz7cr1zi6qfpavj3ddm6dr0966jbgkbvgjz79ljqmpiasf")
+                       (fossil-patch "fossil-fix-json-test.patch"
+                        "fb4e90b662803e47"
+                        "17c01c549e73c6b8"
+                        "12gjzyxs22g9grv7qbgp9jg133bgcsj74621s05fk82j3fc7z59g")
+                       (fossil-patch "fossil-comment-utf-tests.patch"
+                        "5ad708085a90365f"
+                        "fb4e90b662803e47"
+                        "05h2mb6g0840yq74x1cdj95jmqb95i75h6g5v0rzqdc994b96cd4")))))
     (inputs (modify-inputs (package-inputs fossil)
-	      (delete "sqlite")
-	      (append sqlite-next)))))
+             (delete "sqlite")
+             (append sqlite-next)))))
+
+(define-public fossil-bin
+  (package/inherit fossil
+    (name "fossil-bin")
+    (version "2.24")
+    (source (origin
+             (method url-fetch/tarbomb)
+             (uri
+              (string-append
+                "https://fossil-scm.org/home/uv/fossil-linux-x64-" version ".tar.gz"))
+             (sha256 (base32 "0gxsbc9bnbjqdnip162x3di3s4k6dcvd8j8yyzg3jgw7z56hw5sm"))))
+    (build-system binary-build-system)
+    (inputs (list glibc zlib))
+   (arguments
+    '(#:install-plan
+      '(("fossil" "bin/fossil"))
+      #:strip-binaries? #f
+      #:patchelf-plan
+      '(("fossil")
+        ("fossil" ("glibc" "zlib")))))))
