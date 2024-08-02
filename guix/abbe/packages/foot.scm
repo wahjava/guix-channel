5fd1fe81bc02136e6e0bf414f98fdaf95180af24
Ashish SHUKLA <ashish.is@lostca.se>
2024-08-02 17:17:59 +0200

    Update foot to 1.18.0

---
+++ guix/abbe/packages/foot.scm
@@ -0,0 +1,16 @@
+(define-module (abbe packages foot)
+               #:use-module (guix packages)
+               #:use-module (guix git-download)
+               #:use-module (gnu packages terminals))
+
+(define-public foot-1-18
+               (package/inherit foot
+                                (name "foot")
+                                (version "1.18.0")
+                                (source
+                                  (origin (method git-fetch)
+                                          (uri (git-reference
+                                                 (url "https://codeberg.org/dnkl/foot")
+                                                 (commit "1znfk64kimm0vr3alvj66i1yn5glig3bw60n2lv9cn4jzi1b7qhb")))
+                                          (file-name (git-file-name name version))
+                                          (sha256 (base32 (make-string 52 #\0)))))))
