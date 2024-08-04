10b30d861dd09ad57398f2aa4967ecc334d6361a
Ashish SHUKLA <ashish.is@lostca.se>
2024-08-04 23:29:46 +0200

    Update curl-http3 to 8.9.1

--- guix/abbe/packages/curl.scm
+++ guix/abbe/packages/curl.scm
@@ -8,11 +8,21 @@
 
 (define-public curl-http3
   (package/inherit curl-ssh
-                   (arguments (substitute-keyword-arguments (package-arguments curl-ssh)
-                                ((#:configure-flags flags)
-                                 #~(cons* "--with-nghttp3"
-                                          "--with-ngtcp2"
-                                          #$flags))))
-                   (inputs (modify-inputs (package-inputs curl-ssh)
-                                          (prepend nghttp3 ngtcp2)))
-                   (properties `((hidden? . #t)))))
+     (name "curl")
+     (version "8.9.1")
+     (source (origin
+               (method url-fetch)
+               (uri (string-append "https://curl.se/download/curl-"
+                                   version ".tar.xz"))
+               (sha256
+                (base32
+                 "1r8171j91r9ac6byl5yp3mqwib7y5m1mvy2yfazvlnqx0p6gd4pj"))
+               (patches (search-patches "curl-use-ssl-cert-env.patch"))))
+     (arguments (substitute-keyword-arguments (package-arguments curl-ssh)
+                  ((#:configure-flags flags)
+                   #~(cons* "--with-nghttp3"
+                            "--with-ngtcp2"
+                            #$flags))))
+     (inputs (modify-inputs (package-inputs curl-ssh)
+                            (prepend nghttp3 ngtcp2)))
+     (properties `((hidden? . #t)))))
