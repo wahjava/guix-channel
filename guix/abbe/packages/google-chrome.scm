40bde23a7fa71766ee26a97e517e72ee0dbe0b2d
Ashish SHUKLA <ashish.is@lostca.se>
2024-08-14 09:11:39 +0200

    Update google-chrome

--- guix/abbe/packages/google-chrome.scm
+++ guix/abbe/packages/google-chrome.scm
@@ -2,5 +2,5 @@
                #:use-module ((nongnu packages chrome) #:select (make-google-chrome)))
 
 (define-public google-chrome-latest
-               (make-google-chrome "stable" "127.0.6533.99"
-                                   "0n6dxjzvbc5p4nhpa9f68isgfvs42d59j4argkiga2xqm14qphd4"))
+               (make-google-chrome "stable" "127.0.6533.119"
+                                   "0pk6x9h0bzg231zivcdr9gcw9gifg0skjp9d995mff1dn08frnlk"))
