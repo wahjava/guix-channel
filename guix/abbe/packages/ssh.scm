0906bee59188561cabf788c43646da16ccd0de82
Ashish SHUKLA <ashish.is@lostca.se>
2024-09-20 04:55:44 +0000

    Update openssh to 9.9p1

---
+++ guix/abbe/packages/ssh.scm
@@ -0,0 +1,19 @@
+(define-module (abbe packages ssh)
+  #:use-module (guix packages)
+  #:use-module (gnu packages ssh)
+  #:use-module (guix download)
+  #:use-module ((guix licenses) :prefix license:))
+
+(define-public openssh-99
+  (let ((version "9.9p1"))
+    (package
+      (inherit openssh)
+      (name "openssh")
+      (version version)
+      (source (origin
+		            (method url-fetch)
+                (uri (string-append "mirror://openbsd/OpenSSH/portable/"
+                          "openssh-" version ".tar.gz"))
+		            (sha256
+                 (base32 "00kcjs7vm1vha3xvgrkb0qv7v6pwskb1avkfk2qiazzqpz6znhxk"))
+                (patches (origin-patches (package-source openssh))))))))
