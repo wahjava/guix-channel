631f3902e9d5f14c21e7d604ff9a097f1a73680c
Ashish SHUKLA <ashish.is@lostca.se>
2024-09-20 04:58:39 +0000

    Add openssh-sans-x-99

--- guix/abbe/packages/ssh.scm
+++ guix/abbe/packages/ssh.scm
@@ -17,3 +17,17 @@
 		            (sha256
                  (base32 "00kcjs7vm1vha3xvgrkb0qv7v6pwskb1avkfk2qiazzqpz6znhxk"))
                 (patches (origin-patches (package-source openssh))))))))
+
+(define-public openssh-sans-x-99
+  (let ((version "9.9p1"))
+    (package
+      (inherit openssh-sans-x)
+      (name (package-name openssh-sans-x))
+      (version version)
+      (source (origin
+		            (method url-fetch)
+                (uri (string-append "mirror://openbsd/OpenSSH/portable/"
+                          "openssh-" version ".tar.gz"))
+		            (sha256
+                 (base32 "00kcjs7vm1vha3xvgrkb0qv7v6pwskb1avkfk2qiazzqpz6znhxk"))
+                (patches (origin-patches (package-source openssh-sans-x))))))))
