2832bdcdba1232e08a4774dc33f7b6bb5a0adb48
Ashish SHUKLA <ashish.is@lostca.se>
2024-09-28 23:06:37 +0000

    tailscale: switch to source based build

--- guix/abbe/packages/tailscale.scm
+++ guix/abbe/packages/tailscale.scm
@@ -1,47 +1,45 @@
 (define-module (abbe packages tailscale)
   #:use-module (guix packages)
   #:use-module (gnu packages linux)
+  #:use-module (gnu packages golang)
   #:use-module (guix gexp)
-  #:use-module (guix build-system copy)
-  #:use-module (guix download)
-  #:use-module ((guix licenses) :prefix license:)
-  #:use-module (ice-9 match))
-
-(define (tailscale-arch system)
-  (match system
-    ("aarch64-linux" "arm64")
-    ("x86_64-linux" "amd64")
-    ("i686-linux" "386")))
-
-(define (tailscale-url version system)
-  (let ([system (tailscale-arch system)])
-    (string-append "https://pkgs.tailscale.com/stable/tailscale_" version
-                   "_" system ".tgz")))
-
-(define (tailscale-hash system)
-  (match system
-    ("aarch64-linux" "089y6wg8m96kxw0cabdbf1wxl0q8cv1gkc44x6bxljwfmfsid8x7")
-    ("x86_64-linux" "12196z8dhdzxq8mjj0rlvhr6wchmi6z33ym36yqqrp4m52xjz7q5")
-    ("i686-linux" "146vfnj65dd7q7vvh07bis5b26rs3jw385m9wr11jf12q8ad4lsa")))
+  #:use-module (abbe build-system nix-go)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) :prefix license:))
 
 (define-public tailscale
   (package
-   (name "tailscale")
-   (version "1.74.1")
-   (source (origin
-            (method url-fetch/tarbomb)
-            (uri (tailscale-url version (%current-system)))
-            (sha256 (base32 (tailscale-hash (%current-system))))))
-   (build-system copy-build-system)
-   (arguments
-    (list
-     #:install-plan
-     #~`((,(string-append "tailscale_" #$version "_" #$(tailscale-arch (%current-system)) "/") "bin/"))))
-   (propagated-inputs
-                                        ; iptables is required for setting up routing
-    (list iptables))
-   (synopsis "Tailscale connects your team's devices and development environments for easy access to remote resources.")
-   (description
-    "Tailscale is a zero config VPN for building secure networks. Install on any device in minutes. Remote access from any network or physical location.")
-   (home-page "https://tailscale.com/")
-   (license license:bsd-3)))
+    (name "tailscale")
+    (version "1.74.1")
+    (source (origin
+             (method git-fetch)
+             (uri (git-reference
+                   (url "https://github.com/tailscale/tailscale")
+                   (commit (string-append "v" version))))
+             (file-name (git-file-name name version))
+             (sha256 (base32 "0ncck013rzbrzcbpya1fq41jrgzxw22pps77l9kb7kx06as8bggb"))))
+    (build-system nix-go-build-system)
+    (home-page "https://tailscale.com")
+    (propagated-inputs
+     (list iptables))
+    (synopsis "Tailscale connects your team's devices and development environments for easy access to remote resources.")
+    (description
+     "Tailscale is a zero config VPN for building secure networks. Install on any device in minutes. Remote access from any network or physical location.")
+    (license license:bsd-3)
+    (arguments
+     `(#:goarch "arm64"
+       #:goos "linux"
+       #:go ,go-1.23
+       #:sub-packages '("./cmd/tailscaled")
+       #:tags '("ts_include_cli")
+       #:vendor-hash "1wfsk0z43aqi1i2ymhlhzziqc2a96bf2sydg8ppkafw6rl32148w"
+       #:ldflags (list "-w" "-s"
+                  (string-append "-X tailscale.com/version.longStamp=" ,(package-version this-package))
+                  (string-append "-X tailscale.com/version.shortStamp=" ,(package-version this-package)))
+       #:phases
+       ,#~(modify-phases %standard-phases
+           (add-after 'install 'symlink-tailscale
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let* ((out (assoc-ref outputs "out"))
+                      (bin (string-append out "/bin")))
+                 (symlink "tailscaled" (string-append bin "/tailscale"))))))))))
