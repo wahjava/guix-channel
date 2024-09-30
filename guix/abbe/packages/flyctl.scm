28fbd42412fb97327b955502d290b3f407f9a9c9
Ashish SHUKLA <ashish.is@lostca.se>
2024-09-30 11:36:15 +0000

    flyctl: disable auto-updates, and add version information

--- guix/abbe/packages/flyctl.scm
+++ guix/abbe/packages/flyctl.scm
@@ -15,14 +15,23 @@
             (uri (git-reference
                   (url "https://github.com/superfly/flyctl")
                   (commit (string-append "v" version))))
-            (sha256 (base32 "1fh9al65cj07wfr504vzqxr5zcv9qsqq32jr1pcvlmvmcw4vr9p6"))))
+            (sha256 (base32 "1fh9al65cj07wfr504vzqxr5zcv9qsqq32jr1pcvlmvmcw4vr9p6"))
+            (modules '((guix build utils)))
+            (snippet
+             #~(begin
+                 (substitute* "internal/config/config.go"
+                   (("w.AutoUpdate = true")
+                    "w.AutoUpdate = false"))))))
    (build-system nix-go-build-system)
    (arguments
     `(#:go ,go-122
-      #:ldflags '("-X"
+      #:tags '("production")
+      #:ldflags `("-X"
                   "github.com/superfly/flyctl/internal/buildinfo.buildDate=1970-01-01T00:00:00Z"
                   "-X"
-                  "github.com/superfly/flyctl/internal/buildinfo.branchName=master")
+                  "github.com/superfly/flyctl/internal/buildinfo.branchName=master"
+                  "-X"
+                  ,,(string-append "github.com/superfly/flyctl/internal/buildinfo.buildVersion=" version))
      #:vendor-hash "0g5ivrak5vj4kb2knc3ww4i5m1r1xbkgga5dz3gdbkrcc2mr2njz"))
    (synopsis "Command line tools for fly.io services")
    (description
