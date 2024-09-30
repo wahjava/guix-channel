90aed8b51a89c4ffbc498c6f51b568b8940df09f
Ashish SHUKLA <ashish.is@lostca.se>
2024-09-30 12:04:58 +0000

    fzf: switch to nix-go

--- guix/abbe/packages/fzf.scm
+++ guix/abbe/packages/fzf.scm
@@ -2,38 +2,43 @@
   #:use-module (gnu packages)
   #:use-module (guix packages)
   #:use-module (guix gexp)
-  #:use-module (nonguix build-system binary)
-  #:use-module (guix download)
-  #:use-module (ice-9 match)
+  #:use-module (abbe build-system nix-go)
+  #:use-module (guix git-download)
   #:use-module (guix licenses))
 
-(define (fzf-arch system)
-  (match system
-    ("aarch64-linux" "linux_arm64")
-    ("x86_64-linux" "linux_amd64")))
-
-(define (fzf-url version system)
-  (let ([system (fzf-arch system)])
-    (string-append
-      "https://github.com/junegunn/fzf/releases/download/v" version "/fzf-" version "-" system ".tar.gz")))
-
-(define (fzf-hash system)
-  (match system
-    ("x86_64-linux" "1xlc6kcw8mpll5c0rmv5d9zna91jvjzvjaml91p06zrh8s600rv8")
-    ("aarch64-linux" "1vcsxz1zb7gq627rv55hhs02k0g7bwh9p1c220iq61pn2b961jjd")))
+(define %empty-hash (make-string 52 #\0))
 
 (define-public fzf
   (package
    (name "fzf")
-   (version "0.54.3")
+   (version "0.55.0")
    (source (origin
-            (method url-fetch/tarbomb)
-            (uri (fzf-url version (%current-system)))
-            (sha256 (base32 (fzf-hash (%current-system))))))
-   (build-system binary-build-system)
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/junegunn/fzf")
+                  (commit (string-append "v" version))))
+            (sha256 (base32 "0s4df9b2bbf4g32d6yhbdzkzhxmcw3fgl3m1j4fvl5sbx84hsag2"))))
+   (build-system nix-go-build-system)
    (arguments
-    '(#:install-plan
-      '(("fzf" "bin/fzf"))))
+    `(#:vendor-hash "09h5yxh6m9myggb658wfsn8lq43bb1j4prjbn3srl43z69f45f3g"
+      #:build-flags ("-a" "-trimpath")
+      #:ldflags (list "-s" "-w" "-X"
+                       ,(string-append  "main.version=" version)
+                       "-X"
+                       "main.revision=fc693080")
+      #:phases
+      ,#~(modify-phases %standard-phases
+          (add-after 'install 'install-manpages-et-al
+            (lambda* (#:key outputs #:allow-other-keys)
+              (let* ((out (assoc-ref outputs "out"))
+                     (bindir (string-append out "/bin"))
+                     (man1 (string-append out "/share/man/man1")))
+
+                (install-file "man/man1/fzf.1" man1)
+                (install-file "man/man1/fzf-tmux.1" man1)
+
+                (install-file "bin/fzf-tmux" bindir)
+                (install-file "bin/fzf-preview.sh" bindir)))))))
 
    (synopsis "A command-line fuzzy finder")
    (description "A command-line fuzzy finder")
