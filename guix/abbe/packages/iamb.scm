40748c826aa0c301ed91e7551d0d0306a2c5d2e5
Ashish SHUKLA <ashish.is@lostca.se>
2024-10-03 15:19:34 +0000

    iamb: Update to 0.0.10

--- guix/abbe/packages/iamb.scm
+++ guix/abbe/packages/iamb.scm
@@ -1,44 +1,33 @@
 (define-module (abbe packages iamb)
   #:use-module (guix utils)
   #:use-module (guix packages)
+  #:use-module (guix git-download)
   #:use-module (guix gexp)
-  #:use-module (gnu packages base)
-  #:use-module (gnu packages gcc)
-  #:use-module (nonguix build-system binary)
-  #:use-module (guix download)
-  #:use-module (guix licenses)
-  #:use-module (ice-9 match))
-
-(define (iamb-url version system)
-  (match system
-    ("aarch64-linux" (string-append "https://github.com/ulyssa/iamb/releases/download/v" version
-                                    "/iamb-aarch64-unknown-linux-gnu.tgz"))
-    ("x86_64-linux" (string-append "https://github.com/ulyssa/iamb/releases/download/v" version
-                                    "/iamb-x86_64-unknown-linux-musl.tgz"))))
-(define (iamb-hash version system)
-  (match system
-    ("aarch64-linux" "0h0s4ln16msanq9nvqggwyn45bxzf9xbjhyy2md174x93i8n2gql")
-    ("x86_64-linux" "1hwwvi2j4mjxvdk4430l19vw81r3jw5g5fhqcxf0qmgav5bsr2nz")))
+  #:use-module (abbe build-system nix-rust)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages pkg-config)
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public iamb
   (package
     (name "iamb")
     (version "0.0.10")
     (source (origin
-              (method url-fetch)
-              (uri (iamb-url version (%current-system)))
-	            (sha256 (base32 (iamb-hash version (%current-system))))))
-    (build-system binary-build-system)
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/ulyssa/iamb")
+                    (commit (string-append "v" version))))
+	            (sha256 (base32 "026gv6mgaa95qczigyaw8cb4wdyrssajgplrkr5hfzv081cm4c3j"))))
+    (build-system nix-rust-build-system)
     (arguments
-     (list
-      #:install-plan
-      #~`(("iamb" "bin/iamb")
-         ("docs/iamb.5" "share/man/man5/iamb.5")
-         ("docs/iamb.1" "share/man/man1/iamb.1"))
-      #:patchelf-plan
-	    `'(("iamb" ("glibc" "gcc")))))
-    (inputs `(,glibc (,gcc "lib")))
+     `(#:vendor-hash "1k54jxmdwd96zyaymji3ci9bkkrcgyi74k19h0v6rwvy97r4n21f"
+       #:env-vars
+       (list '("OPENSSL_NO_VENDOR" . "yes")
+             '("VERGEN_GIT_SHA" . "2e6376ff866a17eb4727af039b6a8e801da252e2"))))
+    (inputs (list openssl))
+    (native-inputs
+     (list (list "pkg-config" pkg-config)))
     (synopsis "A terminal-based client for Matrix for the Vim addict.")
     (description "iamb is a terminal-based client for Matrix for the Vim addict. You can edit messages, navigate windows and manage tabs in the same ways that your fingers are used to from your favorite text editor!")
     (home-page "https://iamb.chat/")
-    (license asl2.0)))
+    (license license:asl2.0)))
