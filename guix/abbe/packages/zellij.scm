4dcf46cb2bc121e741d64717f19cc87593861e83
Ashish SHUKLA <ashish.is@lostca.se>
2024-10-03 13:27:52 +0000

    zellij: switch to nix-rust

--- guix/abbe/packages/zellij.scm
+++ guix/abbe/packages/zellij.scm
@@ -2,44 +2,35 @@
   #:use-module (gnu packages)
   #:use-module (guix packages)
   #:use-module (guix gexp)
-  #:use-module (nonguix build-system binary)
-  #:use-module (guix download)
-  #:use-module (ice-9 match)
-  #:use-module (guix licenses))
+  #:use-module (abbe build-system nix-rust)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages pkg-config)
+  #:use-module ((guix licenses) #:prefix license:))
 
-(define (zellij-arch system)
-  (match system
-    ("aarch64-linux" "aarch64")
-    ("x86_64-linux" "x86_64")))
-
-(define (zellij-url version system)
-  (let ([system (zellij-arch system)])
-    (string-append
-      "https://github.com/zellij-org/zellij/releases/download/v"
-      version
-      "/zellij-"
-      system
-      "-unknown-linux-musl.tar.gz")))
-
-(define (zellij-hash system)
-  (match system
-    ("aarch64-linux" "0cfd3zjzlqsrxks53fh93zp4l60rk2lg15vhr0l0hcch95mxdw0d")
-    ("x86_64-linux" "03rp4k9hw4f19i498qfy818qp2vi1ndlw0nfw0lxhbxvv7r3xzd9")))
+(define %empty-hash (make-string 52 #\0))
 
 (define-public zellij
   (package
-   (name "zellij")
-   (version "0.40.1")
-   (source (origin
-            (method url-fetch/tarbomb)
-            (uri (zellij-url version (%current-system)))
-            (sha256 (base32 (zellij-hash (%current-system))))))
-   (build-system binary-build-system)
-   (arguments
-    '(#:install-plan
-      '(("zellij" "bin/zellij"))))
+    (name "zellij")
+    (version "0.40.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/zellij-org/zellij")
+                    (commit (string-append "v" version))))
+              (sha256 (base32 "0nbbwwi2v8lpy20y5886rh8zq6l4vjyjqbwvzvq38nwa4yq31iwz"))))
+    (build-system nix-rust-build-system)
+    (native-inputs (list pkg-config))
+    (inputs (list openssl))
+    (arguments
+     `(#:vendor-hash "0588xq2fyd2ax3r3d0awv6lf0508jq3r6nivimb1c8gsp4jf9g68"
+       #:env-vars
+       (list (cons "OPENSSL_NO_VENDOR" "yes")
+             (cons "ZELLIJ_EXAMPLE_DIR" (string-append ,(package-source this-package) "/example"))
+             (cons "ZELLIJ_ASSETS_DIR"  (string-append ,(package-source this-package) "/assets")))))
 
-   (synopsis "A terminal workspace with batteries included")
-   (description "Zellij is a workspace aimed at developers, ops-oriented people and anyone who loves the terminal. Similar programs are sometimes called \"Terminal Multiplexers\".")
-   (home-page "https://zellij.dev")
-   (license expat)))
+    (synopsis "A terminal workspace with batteries included")
+    (description "Zellij is a workspace aimed at developers, ops-oriented people and anyone who loves the terminal. Similar programs are sometimes called \"Terminal Multiplexers\".")
+    (home-page "https://zellij.dev")
+    (license license:expat)))
