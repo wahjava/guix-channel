0f71b062a4e208bdfbd8da08a5a4a4908919a47b
Ashish SHUKLA <ashish.is@lostca.se>
2024-10-03 09:16:02 +0000

    gitui: Switch to nix-rust

--- guix/abbe/packages/gitui.scm
+++ guix/abbe/packages/gitui.scm
@@ -3,41 +3,35 @@
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (guix utils)
-  #:use-module (guix build-system copy)
-  #:use-module (guix download)
-  #:use-module (ice-9 match)
-  #:use-module (guix licenses))
-
-(define (gitui-hash)
-  (match (%current-system)
-    ("aarch64-linux" "19n619wfsa52ny7xh9hv6c6nafh7i61yq8jz0jjp8dvdpdkdj20g")
-    ("x86_64-linux" "1pz27y4c1hxpymd982laim2djnd92c2gc03sw88fg2c7v46mgacm")))
-
-(define (gitui-system)
-  (match (%current-system)
-    ("aarch64-linux" "linux-aarch64")
-    ("x86_64-linux"  "linux-x86_64")))
+  #:use-module (abbe build-system nix-rust)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages ssh)
+;  #:use-module (guix build-utils)
+  #:use-module (gnu packages pkg-config)
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public gitui
   (package
    (name "gitui")
    (version "0.26.3")
-   (source (origin
-            (method url-fetch)
-            (uri 
-             (string-append
-              "https://github.com/extrawurst/gitui/releases/download/v"
-              version
-              "/gitui-"
-              (gitui-system)
-              ".tar.gz"))
-            (sha256 (base32 (gitui-hash)))))
-   (build-system copy-build-system)
+   (source
+    (origin (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/extrawurst/gitui")
+                  (commit (string-append "v" version))))
+            (sha256 (base32 "0afshcj1zspkn4sa92danm9z1ssx7vgnry37l9vdm8xy60mbwz4g"))))
+   (inputs (list openssl libssh2))
+   (native-inputs
+     (list (list "pkg-config" pkg-config)))
+   (build-system nix-rust-build-system)
    (arguments
-    `(#:install-plan
-      ,#~(list
-          (list "gitui" "bin/gitui"))))
+    `(#:vendor-hash "1xhznj6cw7nil26qfxx7pawa7vfllhglgbp2jknnc22y3jznm0xv"
+      #:env-vars
+      '(("OPENSSL_NO_VENDOR" . "yes")
+        ("LIBSSH2_SYS_USE_PKG_CONFIG" . "yes")
+        ("BUILD_GIT_COMMIT_ID" . "95e1d4d4324bf1eab34f8100afc7f3ae7e435252"))))
    (synopsis "Blazing 💥 fast terminal-ui for git written in rust 🦀")
    (description "GitUI provides you with the comfort of a git GUI but right in your terminal")
    (home-page "https://github.com/extrawurst/gitui")
-   (license expat)))
+   (license license:expat)))
