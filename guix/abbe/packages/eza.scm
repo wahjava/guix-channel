9ccf36a458073417a8a7b1cfefb36f9ead0e09c4
Ashish SHUKLA <ashish.is@lostca.se>
2024-10-04 16:08:56 +0000

    eza: switch to nix-rust

--- guix/abbe/packages/eza.scm
+++ guix/abbe/packages/eza.scm
@@ -2,47 +2,27 @@
   #:use-module (gnu packages)
   #:use-module (guix packages)
   #:use-module (guix gexp)
-  #:use-module (nonguix build-system binary)
-  #:use-module (guix download)
-  #:use-module (guix base16)
-  #:use-module (ice-9 match)
-  #:use-module (gnu packages gcc)
-  #:use-module (guix licenses))
-
-(define (eza-arch system)
-  (match system
-    ("aarch64-linux" "aarch64-unknown-linux-gnu")
-    ("x86_64-linux" "x86_64-unknown-linux-gnu")))
-
-(define (eza-url version system)
-  (let ([system (eza-arch system)])
-    (string-append
-     "https://github.com/eza-community/eza/releases/download/v" version "/eza_" system ".tar.gz")))
-
-(define (eza-hash system)
-  (match system
-    ("x86_64-linux" "b4351b3eaa7849aadb005b5769efac120c52a58851c3365fc8de6dcb6721c316")
-    ("aarch64-linux" "14965a8cd5325d8e61e3ea4e12b40c4232d1436728a73242b361cdcdecef1032")))
+  #:use-module (abbe build-system nix-rust)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:))
 
 (define-public eza
   (package
-   (name "eza")
-   (version "0.18.23")
-   (source (origin
-            (method url-fetch/tarbomb)
-            (uri (eza-url version (%current-system)))
-            (sha256 (base16-string->bytevector (eza-hash (%current-system))))))
-   (build-system binary-build-system)
-   (inputs (list (list gcc "lib")))
-   (arguments
-    '(#:install-plan
-      '(("eza" "bin/eza"))
-      #:patchelf-plan
-      `(("eza" ("gcc")))))
+    (name "eza")
+    (version "0.20.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/eza-community/eza")
+                    (commit (string-append "v" version))))
+              (sha256 (base32 "0nz6wcl5pvd3lihd5cc38r77jz466hsri0jwxsdzfzl538zwd6kl"))))
+    (build-system nix-rust-build-system)
+    (arguments
+     `(#:vendor-hash "0l49n1s1ffivf0cbfhmcg4d591658gnglss8bbp1yimdf90w6fnq"))
 
-   (synopsis "A modern alternative to ls.")
-   (description "eza is a modern, maintained replacement for the venerable file-listing
+    (synopsis "A modern alternative to ls.")
+    (description "eza is a modern, maintained replacement for the venerable file-listing
 command-line program \"ls\" that ships with Unix and Linux operating systems,
 giving it more features and better defaults.")
-   (home-page "https://eza.rocks/")
-   (license expat)))
+    (home-page "https://eza.rocks/")
+    (license license:expat)))
